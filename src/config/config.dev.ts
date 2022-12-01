16a6fa84b7115202f23e5da20568ff2bd9eec5c7
Sam Henley <samueljhenley@gmail.com>
2022-12-01 02:02:23 -0500

    remove activity score audit override from dev config

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -5,7 +5,6 @@ const config = globalConfig;
 config.dynamoDb.tableName = 'lounge-dev';
 config.guild.topBalanceRole.roleId = '1043616305942634558';
 
-config.activityScore.balanceReward.scoreAuditInterval = 5000;
 config.activityScore.balanceReward.scoreThreshold = 1;
 config.activityScore.balanceReward.rewardAmount = 1;
 
